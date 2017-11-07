18918c3d5fbb1649668e657d089ccc000342d323
Quentin Le Bian <quentin.lebian.pro@gmail.com>
2017-11-07 09:40:40 +0100

    continue core loop architecture
    finish to build main loop engine to create docs and implements plugins

--- clear.sh
+++ clear.sh
@@ -1,14 +1,17 @@
 # @Description:         This script would to delete dump files
 # @Author:              Quentin Le Bian <quentin>
 # @Email:               quentin.lebian@pilotaweb.fr
-# @Project:             LiveCurrencyConverter
+# @Project:             DocMaker
 # @About:               You're welcome to hack and code as your are each of theses sources files <3:p|--<;
 # @Filename:            clear.sh
 # @Date:                2017-11-04T18:18:19+01:00
-# @Last modified by:    quentin
-# @Last modified time:  2017-11-06T01:12:03+01:00
+# @Last modified by:    quentpilot
+# @Last modified time:  2017-11-06T03:51:50+01:00
 # @License:             MIT
 # @See:                 projects.quentinlebian.fr/LiveCurrencyConverter
 
 
 rm src/core/*/*/**.class
+rm src/core/*.class
+rm src/core/engine/*.class
+rm src/tools/print/*.class
--- open.sh
+++ open.sh
@@ -1,12 +1,12 @@
 # @Description:         This script would to open Live Currency Converter documentation files
 # @Author:              Quentin Le Bian <quentpilot>
 # @Email:               quentin.lebian@pilotaweb.fr
-# @Project:             LiveCurrencyConverter
+# @Project:             DocMaker
 # @About:               You're welcome to hack and code as your are each of theses sources files <3:p|--<;
 # @Filename:            open.apidoc.sh
 # @Date:                2017-11-04T16:51:24+01:00
-# @Last modified by:    quentin
-# @Last modified time:  2017-11-06T01:41:28+01:00
+# @Last modified by:    quentpilot
+# @Last modified time:  2017-11-06T03:38:40+01:00
 # @License:             MIT
 # @See:                 projects.quentinlebian.fr/LiveCurrencyConverter
 
